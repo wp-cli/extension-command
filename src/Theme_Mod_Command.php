fce7ec0af3319a5d78b76b32bc8653ab63d8078a
Mitesh <mitesh.thakor@rtcamp.com>
2018-05-27 14:16:09 +0530

    Added new command

--- src/Theme_Mod_Command.php
+++ src/Theme_Mod_Command.php
@@ -122,6 +122,52 @@ class Theme_Mod_Command extends WP_CLI_Command {
 	}
 
 	/**
+	 * Gets all theme mods.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [<mod>...]
+	 * : One or more mods to get.
+	 *
+	 * [--field=<field>]
+	 * : Returns the value of a single field.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - json
+	 *   - csv
+	 *   - yaml
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Get all theme mods.
+	 *     $ wp theme mod list
+	 *     +------------------+---------+
+	 *     | key              | value   |
+	 *     +------------------+---------+
+	 *     | background_color | dd3333  |
+	 *     | link_color       | #dd9933 |
+	 *     | main_text_color  | #8224e3 |
+	 *     +------------------+---------+
+	 *
+	 */
+	public function list( $args = array(), $assoc_args = array() ) {
+		if ( ! array_key_exists( 'all', $assoc_args ) ) {
+			$assoc_args['all'] = 1;
+		}
+		// If no format was given ,set to default 'table'.
+		if ( ! array_key_exists( 'format', $assoc_args ) ) {
+			$assoc_args['format'] = 'table';
+		}
+		$this->get( $args, $assoc_args );
+	}
+
+	/**
 	 * Removes one or more theme mods.
 	 *
 	 * ## OPTIONS
