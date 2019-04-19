e454b794f0543c4df59033c018f8ee5034fc22a6
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-19 21:40:24 +0530

    PHPCS: Fix up errors in extension-command.php

--- extension-command.php
+++ extension-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_extension_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_extension_autoloader ) ) {
+	require_once $wpcli_extension_autoloader;
 }
 
 WP_CLI::add_command( 'plugin', 'Plugin_Command' );
