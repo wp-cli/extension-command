610e8491c327b0a43f384f3ff68387f8cb257bcc
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-19 18:12:14 +0530

    PHPCS: Fix up errors src/WP_CLI/DestructivePluginUpgrader.php src/WP_CLI/DestructiveThemeUpgrader.php

--- src/WP_CLI/DestructivePluginUpgrader.php
+++ src/WP_CLI/DestructivePluginUpgrader.php
@@ -8,9 +8,9 @@ namespace WP_CLI;
 class DestructivePluginUpgrader extends \Plugin_Upgrader {
 
 	public function install_package( $args = array() ) {
-		parent::upgrade_strings();  // needed for the 'remove_old' string
+		parent::upgrade_strings(); // needed for the 'remove_old' string.
 
-		$args['clear_destination'] = true;
+		$args['clear_destination']           = true;
 		$args['abort_if_destination_exists'] = false;
 		return parent::install_package( $args );
 	}
