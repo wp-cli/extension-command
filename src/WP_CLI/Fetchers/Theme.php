213d96fd9ffb38c74c024057c355432208b80af7
Robert Collins <beau@collins.pub>
2019-09-13 15:31:46 -0400

    Fix code style

--- src/WP_CLI/Fetchers/Theme.php
+++ src/WP_CLI/Fetchers/Theme.php
@@ -21,7 +21,7 @@ class Theme extends Base {
 	public function get( $name ) {
 		// Workaround to equalize folder naming conventions across Win/Mac/Linux
 		// Returns false if theme stylesheet doesn't exactly match existing themes.
-		$existing_themes = wp_get_themes( array( 'errors' => null ) );
+		$existing_themes      = wp_get_themes( array( 'errors' => null ) );
 		$existing_stylesheets = array_keys( $existing_themes );
 		if ( ! in_array( $name, $existing_stylesheets, true ) ) {
 			return false;
