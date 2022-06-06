4ba51328d1f27fb1955af78dc0f64952fab4e582
Niels de Blaauw <niels@level-level.com>
2022-06-06 12:39:46 +0200

    Fixes results output.

--- src/Plugin_Command.php
+++ src/Plugin_Command.php
@@ -924,11 +924,11 @@ class Plugin_Command extends \WP_CLI\CommandWithUpgrade {
 			if ( Utils\get_flag_value( $assoc_args, 'deactivate' ) ) {
 				WP_CLI::log( "Deactivating '{$plugin->name}'..." );
 				$this->chained_command = true;
-				$args                  = [];
+				$deactivate_args       = [];
 				if ( is_plugin_active_for_network( $plugin->file ) ) {
-					$args['network'] = true;
+					$deactivate_args['network'] = true;
 				}
-				$this->deactivate( array( $plugin->name ), $args );
+				$this->deactivate( array( $plugin->name ), $deactivate_args );
 				$this->chained_command = false;
 			}
 
