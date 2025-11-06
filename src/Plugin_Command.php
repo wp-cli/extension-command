075ea64c2eef57a26a24beab832c49b84a57f824
Pascal Birchler <pascalb@google.com>
2025-11-06 19:01:28 +0100

    Revert "Remove `url` field from plugin command docs"
    This reverts commit 383c3079d87573cfd2bd590b54c5143eb9da113d.

--- src/Plugin_Command.php
+++ src/Plugin_Command.php
@@ -170,6 +170,7 @@ class Plugin_Command extends CommandWithUpgrade {
 	 *     **icons**: Plugin's Icon Image Link
 	 *     **active_installs**: Plugin's Number of Active Installs
 	 *     **contributors**: Plugin's List of Contributors
+	 *     **url**: Plugin's URL on wordpress.org
 	 *
 	 * [--format=<format>]
 	 * : Render output in a particular format.
