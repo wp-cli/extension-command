4b13d6cb7e7ae3840cf2271ca1723f0847261118
Pascal Birchler <pascalb@google.com>
2026-02-10 18:04:14 +0100

    Apply suggestions

--- src/WP_CLI/ExtensionUpgraderSkin.php
+++ src/WP_CLI/ExtensionUpgraderSkin.php
@@ -8,6 +8,9 @@ use WP_CLI;
  * An Upgrader Skin for extensions (plugins/themes) that displays which item is being updated
  *
  * @package wp-cli
+ *
+ * @property-read array{Name?: string}|null $plugin_info
+ * @property-read \WP_Theme|null $theme_info
  */
 class ExtensionUpgraderSkin extends UpgraderSkin {
 
@@ -16,11 +19,8 @@ class ExtensionUpgraderSkin extends UpgraderSkin {
 	 */
 	public function before() {
 		// These properties are defined in `Bulk_Plugin_Upgrader_Skin`/`Bulk_Theme_Upgrader_Skin`
-		// @phpstan-ignore property.notFound
 		if ( isset( $this->plugin_info ) && is_array( $this->plugin_info ) && isset( $this->plugin_info['Name'] ) ) {
-			// @phpstan-ignore argument.type
 			WP_CLI::log( sprintf( 'Updating %s...', html_entity_decode( $this->plugin_info['Name'], ENT_QUOTES, get_bloginfo( 'charset' ) ) ) );
-			// @phpstan-ignore property.notFound
 		} elseif ( isset( $this->theme_info ) && is_object( $this->theme_info ) && method_exists( $this->theme_info, 'get' ) ) {
 			WP_CLI::log( sprintf( 'Updating %s...', html_entity_decode( $this->theme_info->get( 'Name' ), ENT_QUOTES, get_bloginfo( 'charset' ) ) ) );
 		}
