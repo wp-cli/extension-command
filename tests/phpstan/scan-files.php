85c029a07d1bd7da4f8db5b1463f4fa723a781e0
Pascal Birchler <pascalb@google.com>
2026-06-10 13:52:29 +0200

    Add PHPStan stubs

---
+++ tests/phpstan/scan-files.php
@@ -0,0 +1,14 @@
+<?php
+// phpcs:ignoreFile
+
+namespace {
+
+	class Requests_Response {
+		/** @var bool */
+		public $success;
+		/** @var int */
+		public $status_code;
+		/** @var string */
+		public $body;
+	}
+}
